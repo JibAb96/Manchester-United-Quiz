f5ee56e0e98a851022efd8693832d4ad807c2c72
JibAb96 <j.abshir96@gmail.com>
2024-05-31 06:51:57 +0000

    Quiz questions array added to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// An array containing the questions, options and correct answers for the quiz
+const quizData = [
+    {
+        question: "How many goals did Cristiano Ronaldo score to win the Ballon D'or?",
+        options: ["71","63","51","42"],
+        answer: "42"
+    },
+    {
+        question: "What club did Manchester United loan Carlos Tevez from?",
+        options: ["Barcelona", "West Ham", "Bayern Munich", "AC Milan"],
+        answer: "West Ham"
+    },
+    {
+        question: "Who scored the iconic and only goal in the Champions League Semi Final against Barcelona?",
+        options: ["Paul Scholes", "Dimitar Berbatov", "Cristiano Ronaldo", "Wayne Rooney"],
+        answer: "Paul Scholes"
+
+    },
+    {
+        question: "Who did Manchester United go toe to toe with in the Premier League before winning the title?",
+        options: ["Arsenal", "Liverpool", "Chelsea", "Manchester City"],
+        answer: "Chelsea"
+    },
+    {
+        question: "Who did Manchester United go out to in the FA Cup?",
+        options: ["Arsenal", "Leyton Orient", "Chelsea", "Portsmouth"],
+        answer: "Portsmouth"
+    }
+]
